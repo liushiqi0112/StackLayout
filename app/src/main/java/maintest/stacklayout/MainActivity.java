56995da308ca580aa92fd7a69bb6c07cfe22af20
liushiqi0112 <1334046571@qq.com>
2017-08-23 11:33:13 +0800

    Initial commit

---
+++ app/src/main/java/maintest/stacklayout/MainActivity.java
@@ -0,0 +1,150 @@
+package maintest.stacklayout;
+
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fashare.stack_layout.StackLayout;
+import com.fashare.stack_layout.transformer.AngleTransformer;
+import com.fashare.stack_layout.transformer.StackPageTransformer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    static List<Integer> sRandomColors = new ArrayList<>();
+    static{
+        for(int i=0; i<10; i++)
+            sRandomColors.add(new Random().nextInt() | 0xff000000);
+    }
+
+    SwipeRefreshLayout mRefreshLayout;
+    StackLayout mStackLayout;
+    Adapter mAdapter;
+
+    List<String> mData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        loadData(0);
+    }
+
+    private void initView() {
+        mRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.srl_refresh);//下拉刷新控件
+        mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        int page = 0;
+                        curPage = 0;
+                        mData = new ArrayList<>(Arrays.asList(String.valueOf(page*3), String.valueOf(page*3+1), String.valueOf(page*3+2)));
+                        mStackLayout.setAdapter(mAdapter = new Adapter(mData));
+                        mRefreshLayout.setRefreshing(false);
+                    }
+                }, 1000);
+            }
+        });
+        mRefreshLayout.setColorSchemeResources(R.color.red,
+                R.color.blue,
+                R.color.green,
+                R.color.orange);
+
+        mStackLayout = (StackLayout) findViewById(R.id.stack_layout);
+        mStackLayout.setAdapter(mAdapter = new Adapter(mData = new ArrayList<>()));
+        mStackLayout.addPageTransformer(
+                new StackPageTransformer(),     // 堆叠
+                new MyAlphaTransformer(),       // 渐变
+                new AngleTransformer()          // 角度
+        );
+
+        mStackLayout.setOnSwipeListener(new StackLayout.OnSwipeListener() {
+            @Override
+            public void onSwiped(View swipedView, int swipedItemPos, boolean isSwipeLeft, int itemLeft) {
+                toast((isSwipeLeft? "往左": "往右") + "移除" + mData.get(swipedItemPos) + "." + "剩余" + itemLeft + "项");
+
+                // 少于5条, 加载更多
+                if(itemLeft < 5){
+                    loadData(++ curPage);
+                }
+            }
+        });
+    }
+
+    int curPage = 0;
+
+    private void loadData(final int page) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mAdapter.getData().addAll(Arrays.asList(String.valueOf(page*3), String.valueOf(page*3+1), String.valueOf(page*3+2)));
+                mAdapter.notifyDataSetChanged();
+            }
+        }, 1000);
+    }
+
+    class Adapter extends StackLayout.Adapter<Adapter.ViewHolder>{
+        List<String> mData;
+
+        public void setData(List<String> data) {
+            mData = data;
+        }
+
+        public List<String> getData() {
+            return mData;
+        }
+
+        Adapter(List<String> data) {
+            mData = data;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int position) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, final int position) {
+            holder.mTextView.setText(mData.get(position));
+            holder.itemView.findViewById(R.id.layout_content).setBackgroundColor(sRandomColors.get(position%sRandomColors.size()));
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
+
+        class ViewHolder extends StackLayout.ViewHolder{
+            TextView mTextView;
+            ViewHolder(View itemView) {
+                super(itemView);
+                mTextView = itemView.findViewById(R.id.tv);
+            }
+        }
+
+    }
+
+    void toast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
